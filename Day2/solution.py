bd0b05af02a3d0f1e9c4cd484feaf41f5cf1fd00
SeaSaltShaker <kaylasd10@yahoo.com>
2024-12-05 21:26:00 -0500

    Day 2 part 1 solution

---
+++ Day2/solution.py
@@ -0,0 +1,33 @@
+import pathlib
+curdir = pathlib.Path(__file__).parent.resolve()
+
+count = 0
+
+def validate(array):
+    increasing = None
+    if array[1] - array[0] > 0:
+        increasing = True
+    elif array[1] - array[0] < 0:
+        increasing = False
+    else:
+        return False
+    for i in range(0, len(array) - 1):
+        diff = array[i+1] - array[i]
+        if increasing and (1 <= diff <= 3):
+            continue
+        elif increasing:
+            return False
+        elif not increasing and (-3 <= diff <= -1):
+            continue
+        else:
+            return False
+    return True
+
+with open(f"{curdir}/input.txt", "r") as fp:
+    for line in fp:
+        array = list(map(int, line.split()))
+        if(validate(array)):
+            count += 1
+        
+
+print(count)
